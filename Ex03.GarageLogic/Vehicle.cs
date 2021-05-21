869be86c30e1d511d6e1145d65a2cd9363abe884
Danielle Hasson <danielleh9.6@gmail.com>
2021-05-21 14:17:18 +0300

    vehicle component update

--- Ex03.GarageLogic/Vehicle.cs
+++ Ex03.GarageLogic/Vehicle.cs
@@ -10,11 +10,11 @@ namespace Ex03.GarageLogic
     {
         internal class Wheel
         {
-            private readonly string mr_ManufactureName;
+            private readonly string mr_ManufacturerName;
             private float m_CurAirPressure;
             private float m_MaxAirPressure;
 
-            internal Wheel(string i_ManufactureName, float i_CurAirPressure, float i_MaxAirPressure)
+            internal Wheel(string i_ManufacturerName, float i_CurAirPressure, float i_MaxAirPressure)
             {
                 if (i_CurAirPressure > i_MaxAirPressure || i_CurAirPressure < 0)
                 {
@@ -22,7 +22,7 @@ namespace Ex03.GarageLogic
                 }
                 else
                 {
-                    this.mr_ManufactureName = i_ManufactureName;
+                    this.mr_ManufacturerName = i_ManufacturerName;
                     this.m_CurAirPressure = i_CurAirPressure;
                     this.m_MaxAirPressure = i_MaxAirPressure;
                 }
@@ -54,20 +54,47 @@ namespace Ex03.GarageLogic
                 string wheelToString = string.Format(@"Manufacturer Name:  {0}
                                                         Maximum Air Pressure: {1}
                                                         Current Air Pressure: {2}"
-                                                        , mr_ManufactureName, m_MaxAirPressure, m_CurAirPressure);
+                                                        , mr_ManufacturerName, m_MaxAirPressure, m_CurAirPressure);
                 return wheelToString;
             }
         }
 
         protected readonly string mr_ModelName;
         protected readonly string mr_LicensePlateNumber;
-        protected float m_EnergyPrecent;
+        protected float m_RemainingEnergyPrecent;
         protected readonly bool mvr_IsFuelBased;
         protected Wheel[] m_Wheels;
+        protected float m_MaxAirPressure;
+        protected readonly eFuelType mr_FuelType;
 
-        public Vehicle(string i_ModelName, string i_LicensePlateNumber, float i_EnergyPrecent, bool i_IsFuelBased)
+
+        internal Vehicle(string i_ModelName, string i_LicensePlateNumber, float i_RemainingEnergyPrecent, bool i_IsFuelBased, eFuelType i_FuelType, int i_NumOfWheels, float i_MaxAirPressure)
         {
+            this.mr_ModelName = i_ModelName;
+            this.mr_LicensePlateNumber = i_LicensePlateNumber;
+            this.m_RemainingEnergyPrecent = i_RemainingEnergyPrecent;
+            this.m_Wheels = new Wheel[i_NumOfWheels];
+            this.m_MaxAirPressure = i_MaxAirPressure;
+            this.mvr_IsFuelBased = i_IsFuelBased;
 
+            if(this.mvr_IsFuelBased)
+            {
+                this.mr_FuelType = i_FuelType;
+            }
+
+
+            
+        }
+
+        /*
+         * Set each wheel of the vehicle
+        */
+        internal void setWheels(string i_ManufacturerName, float i_CurAirPressure)
+        {
+            for(int i = 0; i < this.m_Wheels.Length; i++)
+            {
+                this.m_Wheels[i] = new Wheel(i_ManufacturerName, i_CurAirPressure, this.m_MaxAirPressure);
+            }
         }
     }
 }
